2ff8c470457c23fbf880415c45d34a9085ede20a
Narogos <40336047+Narogo@users.noreply.github.com>
2020-10-11 18:23:26 +0200

    Update pizzas.sql

--- baza/pizzas.sql
+++ baza/pizzas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 11 Paź 2020, 10:55
+-- Czas generowania: 11 Paź 2020, 18:22
 -- Wersja serwera: 10.4.13-MariaDB
 -- Wersja PHP: 7.2.32
 
@@ -20,6 +20,83 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `pizzahouse`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `kebabs`
+--
+
+CREATE TABLE `kebabs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `size` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cake` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `meat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sauce` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Zrzut danych tabeli `kebabs`
+--
+
+INSERT INTO `kebabs` (`id`, `created_at`, `updated_at`, `name`, `size`, `cake`, `meat`, `sauce`) VALUES
+(1, '2020-10-11 09:27:54', '2020-10-11 09:27:54', 'test', 'Mały', 'Arabskie', 'Kurczak', 'Lagodny'),
+(2, '2020-10-11 09:30:31', '2020-10-11 09:30:31', 'test2', 'Średni', 'Chleb', 'Wołowina', 'Ostry'),
+(3, '2020-10-11 09:30:45', '2020-10-11 09:30:45', 'test 3', 'Duży', 'Chleb', 'Mieszane', 'Mieszany');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Zrzut danych tabeli `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(8, '2014_10_12_000000_create_users_table', 1),
+(9, '2019_08_19_000000_create_failed_jobs_table', 1),
+(10, '2020_08_14_173013_create_pizzas_table', 1),
+(11, '2020_08_14_174700_add_price_to_pizzas_table', 1),
+(14, '2014_10_12_100000_create_password_resets_table', 2),
+(19, '2020_10_11_102802_create_kebab_tables', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -47,9 +124,57 @@ INSERT INTO `pizzas` (`id`, `created_at`, `updated_at`, `type`, `base`, `name`, 
 (21, '2020-10-09 18:06:50', '2020-10-09 18:06:50', 'pepperoni', 'Grube', 'test 2', '[\"pieczarki\",\"papryczka chili\",\"cebula\",\"papryka\",\"kurczak\"]', 20),
 (22, '2020-10-09 18:07:17', '2020-10-09 18:07:17', 'szynkowa', 'Cienkie', 'test 3', '[\"pieczarki\",\"papryka\"]', 20);
 
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, '123', '123@123', NULL, '$2y$10$YPFvtzFvnKYdbmEI7kzGNO.EiqSbNuzNNwB5kCGeuUalC0/zGtW4y', 'bIKz9HVzklUYTpYaXYpH2XOjn2uScPHeVGI7BieTg06t3GBNqEPD6Ie5odcv', '2020-08-19 12:15:36', '2020-08-19 12:15:36');
+
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `kebabs`
+--
+ALTER TABLE `kebabs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
 
 --
 -- Indeksy dla tabeli `pizzas`
@@ -58,14 +183,45 @@ ALTER TABLE `pizzas`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT dla tabeli `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `kebabs`
+--
+ALTER TABLE `kebabs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT dla tabeli `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT dla tabeli `pizzas`
 --
 ALTER TABLE `pizzas`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
